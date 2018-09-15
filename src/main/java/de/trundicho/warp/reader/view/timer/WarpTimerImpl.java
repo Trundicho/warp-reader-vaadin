9b62bb4da07b268a66971b975eac3ec7bfa7e7a8
Trundicho <romito@web.de>
2018-09-15 16:43:39 +0200

    Removes unused field

--- src/main/java/de/trundicho/warp/reader/view/timer/WarpTimerImpl.java
+++ src/main/java/de/trundicho/warp/reader/view/timer/WarpTimerImpl.java
@@ -6,11 +6,9 @@ import de.trundicho.warp.reader.core.view.api.timer.WarpTimer;
 import de.trundicho.warp.reader.view.WarpReaderView;
 
 class WarpTimerImpl implements WarpTimer {
-    private final PlayModel playModel;
     private final Timer timer;
 
     WarpTimerImpl(PlayModel playModel, WarpReaderView ui) {
-        this.playModel = playModel;
         timer = new Timer();
         timer.run(() -> doNextWarp(playModel));
         ui.addExtension(timer);
