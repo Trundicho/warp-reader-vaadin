d0e601d7eb5d65eac8d3e7997d4f97e64359353d
Angelo Romito <romito@web.de>
2018-09-21 23:49:25 +0200

    Workaround: WarpReaderCore gets a real gwt entry-point

---
+++ warp-reader-core/src/main/java/de/trundicho/warp/reader/core/view/api/WarpReaderCore.java
@@ -0,0 +1,10 @@
+package de.trundicho.warp.reader.core.view.api;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class WarpReaderCore implements EntryPoint {
+
+	public void onModuleLoad() {
+
+	}
+}
